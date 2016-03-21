de5dd084ed4d4b85eb5f56945fe3642ab1622660
10amc-2 <ba228jw@gmail.com>
2016-03-21 14:55:35 -0400

    Consolidated preprocessing and imputation into single class
    still need to add roll up and clean up

--- knn_impute.py
+++ knn_impute.py
@@ -29,108 +29,112 @@ logger2 = logging.getLogger(__name__)
 
 #logging.basicConfig(filename='mp_debug.log',level=logging.DEBUG)
 
-def find_dups(l):
-    newl = []
-    dups = []
-    for entry in l:
-        if entry in newl:
-            dups.append(entry)
-        else:
-            newl.append(entry)
-    return dups
+class imputable:
 
-def drop_dups(df, dups):
-    newdf = df.drop(dups)
-    return newdf
+    def __init__(self, filename, missingness):
+        self.data = pd.read_csv(filename,sep='\t')
+        self.miss = missingness
 
-def take(n, iterable):
-    "Return first n items of the iterable as a list"
-    return list(islice(iterable, n))
+    def deduplicate(self):
+        if self.data[self.data.columns.values[1]][0][-2] == "T":
+            self.data[self.data.columns.values[1]] = self.data[self.data.columns.values[1]].str.replace('T*', '')
+            self.data = self.data.groupby(['Peptide','Protein']).mean()
+        todrop = []
+        for name, group in self.data.groupby(level='Peptide'):
+            if len(group) > 1:
+                todrop.append(name)
+        self.data = self.data.drop(todrop)
 
-def match_pat(l,d,i):
-    l = "".join(np.isnan(l).astype(int).astype(str))
-    if l not in d.keys():
-        d[l] = [i]
-    else:
-        d[l].append(i)
+    def drop_missing(self):
+        self.miss = np.rint(len(self.data.columns)*self.miss)
+        self.data = self.data[self.data.isnull().sum(axis=1)<=self.miss]
 
-def get_patterns(arr):
-    patts = {}
-    for ind, val in enumerate(arr):
-        match_pat(val,patts,ind)
-    return patts
+    def impute(self):
+        def match_pat(l,d,i):
+            l = "".join(np.isnan(l).astype(int).astype(str))
+            if l not in d.keys():
+                d[l] = [i]
+            else:
+                d[l].append(i)
 
-def sub_imputer(key,pattern,origarr,comparr):
-    newarr = comparr[:,~np.array(list(pattern)).astype(bool)]
-    nbrs = NearestNeighbors(n_neighbors=10).fit(newarr)
-    indexes = nbrs.kneighbors([origarr[key,~np.array(list(pattern)).astype(bool)]],return_distance=False)
-    means = np.mean(comparr[indexes[0][1:]], axis=0)
-    outl = []
-    for ind, v in enumerate(origarr[key]):
-        if not np.isnan(v):
-            outl.append(v)
-        else:
-            outl.append(means[ind])
-    logging.debug("imputed %d" % key)
-    return outl
+        def get_patterns(arr):
+            patts = {}
+            for ind, val in enumerate(arr):
+                match_pat(val,patts,ind)
+            return patts
 
+        def sub_imputer(key,pattern,origarr,comparr):
+            newarr = comparr[:,~np.array(list(pattern)).astype(bool)]
+            nbrs = NearestNeighbors(n_neighbors=10).fit(newarr)
+            indexes = nbrs.kneighbors([origarr[key,~np.array(list(pattern)).astype(bool)]],return_distance=False)
+            means = np.mean(comparr[indexes[0][1:]], axis=0)
+            outl = []
+            for ind, v in enumerate(origarr[key]):
+                if not np.isnan(v):
+                    outl.append(v)
+                else:
+                    outl.append(means[ind])
+            logging.debug("imputed %d" % key)
+            return outl
 
-def mp_imputer(patdict, origarr, comparr, nprocs):
-    def worker(patdict, keys, origarr, comparr, out_q):
-        """ The worker function, invoked in a process. 'nums' is a
-            list of numbers to factor. The results are placed in
-            a dictionary that's pushed to a queue.
-        """
-        outdict = {}
-        n = 0
-        for k in keys:
-            outdict[k] = sub_imputer(k,patdict[k], origarr,comparr)
-            n += 1
-            logging.info("%d / %d" % (n,len(keys)))
-        with open('results_%d.txt' % out_q, "w") as f:
-                   json.dump(outdict, f)
-                   #f.write("\n")
-    # Each process will get 'chunksize' nums and a queue to put his out
-    # dict into
-    out_q = Queue()
-    chunksize = int(math.ceil(len(patdict.keys()) / float(nprocs)))
-    procs = []
+        def mp_imputer(patdict, origarr, comparr, nprocs):
+            def worker(patdict, keys, origarr, comparr, out_q):
+                """ The worker function, invoked in a process. 'nums' is a
+                    list of numbers to factor. The results are placed in
+                    a dictionary that's pushed to a queue.
+                """
+                outdict = {}
+                n = 0
+                for k in keys:
+                    outdict[k] = sub_imputer(k,patdict[k], origarr,comparr)
+                    n += 1
+                    logging.info("%d / %d" % (n,len(keys)))
+                with open('results_%d.txt' % out_q, "w") as f:
+                           json.dump(outdict, f)
+                               #f.write("\n")
+                # Each process will get 'chunksize' nums and a queue to put his out
+                # dict into
+            out_q = Queue()
+            chunksize = int(math.ceil(len(patdict.keys()) / float(nprocs)))
+            procs = []
 
-    for i in range(nprocs):
-        p = multiprocessing.Process(
-                target=worker,
-                args=(patdict,patdict.keys()[chunksize * i:chunksize * (i + 1)], origarr, comparr,
-                      i))
-        procs.append(p)
-        logging.info("Starting process %d" % i)
-        p.start()
+            for i in range(nprocs):
+                p = multiprocessing.Process(
+                        target=worker,
+                        args=(patdict,patdict.keys()[chunksize * i:chunksize * (i + 1)], origarr, comparr,
+                              i))
+                procs.append(p)
+                logging.info("Starting process %d" % i)
+                p.start()
 
-    # # Collect all results into a single result dict. We know how many dicts
-    # # with results to expect.
-    # resultdict = {}
-    # logging.info("getting output")
-    # for i in range(nprocs):
-    #     resultdict.update(out_q.get())
-    #     logging.info("got dict %d" % i)
-    # # Wait for all worker processes to finish
-    # for p in procs:
-    #     logging.info("joining dict %d" % i)
-    #     p.join()
+                # # Collect all results into a single result dict. We know how many dicts
+                # # with results to expect.
+                # resultdict = {}
+                # logging.info("getting output")
+                # for i in range(nprocs):
+                #     resultdict.update(out_q.get())
+                #     logging.info("got dict %d" % i)
+                # # Wait for all worker processes to finish
+                # for p in procs:
+                #     logging.info("joining dict %d" % i)
+                #     p.join()
 
-    return
+            return
+
+        datavals = self.data.drop(self.data.columns[:3],axis=1).values
+        comparr = datavals[~np.isnan(datavals).any(axis=1)]
+        patterns = get_patterns(datavals)
+        revpatterns = {i:x for x,y in patterns.iteritems() for i in y}
+        mp_imputer(revpatterns, datavals, comparr, 7)
 
 
 
 if __name__ == '__main__':
     # Define the parameters to test
-    data = pd.read_csv('Deduplicated_for_imputation.txt', sep = '\t')
-    datacomp = data[data.isnull().sum(axis=1)<=64]
-    datavals = datacomp.drop(data.columns[:3],axis=1).values
-    comparr = datavals[~np.isnan(datavals).any(axis=1)]
-    patterns = get_patterns(datavals)
-    revpatterns = {i:x for x,y in patterns.iteritems() for i in y}
-
-    mp_imputer(revpatterns, datavals, comparr, 7)
+    to_impute = imputable('NonUniquePeptideResults.txt',0.3)
+    to_impute.deduplicate()
+    to_impute.drop_missing()
+    to_impute.impute()
 
 # data={}
 #
