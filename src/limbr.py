7c303e069b03619f97bf0d8f9b8f4d2a75de7ae1
aleccrowell <ba228jw@gmail.com>
2017-03-14 00:14:00 -0400

    fixed or statement

--- src/limbr.py
+++ src/limbr.py
@@ -132,7 +132,7 @@ class sva:
                 df = df.sort_values(df.columns[i])
                 df[df.columns[i]] = ref
             return df.sort_index()
-        if self.data_type == 'r' | self.norm_map == None:
+        if (self.data_type == 'r') or (self.norm_map == None):
             self.data = qnorm(self.raw_data)
             self.scaler = preprocessing.StandardScaler().fit(self.data.values.T)
             self.data = pd.DataFrame(self.scaler.transform(self.data.values.T).T,columns=self.data.columns,index=self.data.index)
