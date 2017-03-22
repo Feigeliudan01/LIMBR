c4a3e1e9356c5da510435b12ab7bc644aab05ead
aleccrowell <ba228jw@gmail.com>
2017-03-21 22:31:14 -0400

    switched pool flag to j

--- src/old_normalize.py
+++ src/old_normalize.py
@@ -6,14 +6,14 @@ def main(argv):
     inputfile = ''
     outputfile = ''
     try:
-        opts, args = getopt.getopt(argv,"h:i:o:s:n:p:a:d:e:pp:",["help","ifile=","ofile=","experiment=","pools="])
+        opts, args = getopt.getopt(argv,"h:i:o:s:n:p:a:d:e:j:",["help","ifile=","ofile=","experiment=","pools="])
     except getopt.GetoptError:
-        print('residuals.py -i <inputfile> -o <outputfile> -e <experimenttype> -pp <pdesignpath>')
+        print('residuals.py -i <inputfile> -o <outputfile> -e <experimenttype> -j <pdesignpath>')
         sys.exit(2)
     b = None
     for opt, arg in opts:
         if opt in ('-h',"--help"):
-            print('residuals.py -i <inputfile> -o <outputfile> -e <experimenttype> -pp <pdesignpath>')
+            print('residuals.py -i <inputfile> -o <outputfile> -e <experimenttype> -j <pdesignpath>')
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
@@ -21,7 +21,7 @@ def main(argv):
             outputfile = arg
         elif opt in ("-e", "--experiment"):
             e = arg
-        elif opt in ("-pp", "--pools"):
+        elif opt in ("-j", "--pools"):
             pool = arg
     print('reading data')
     to_old = old_fashioned(inputfile,e,pool)
