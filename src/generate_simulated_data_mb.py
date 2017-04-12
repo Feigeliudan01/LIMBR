06eb77d4aee6ee47564a64ce86e1ee6afa54812f
aleccrowell <ba228jw@gmail.com>
2017-04-12 19:17:03 -0400

    more mb sims

--- src/generate_simulated_data_mb.py
+++ src/generate_simulated_data_mb.py
@@ -76,5 +76,5 @@ def gen_sim_data(suf):
     trends=pd.DataFrame([basetrend,basetrend2,basetrend3])
     trends.to_csv('output/simdata/mb_trends_'+str(m)+'.txt',sep='\t')
 
-for m in range(1,6):
+for m in range(1,11):
     gen_sim_data(m)
