ae6c485ef19d296eb6825b119a99dd5fb2aff802
aleccrowell <alexader.m.crowell@gmail.com>
2018-01-10 13:11:26 -0500

    Upgraded imputable.py docstrings to numpy format

--- pypi/LIMBR/imputable.py
+++ pypi/LIMBR/imputable.py
@@ -21,19 +21,41 @@ from functools import partial
 from sklearn import preprocessing
 
 class imputable:
+    """
+    Imputes missing data with K Nearest Neighbors based on user specified parameters.
+
+    This class generates an object from raw data and allows for preprocessing, imputation and output of that data.
+
+    Parameters
+    ----------
+    filename : str
+        This is the path to the file containing the raw data.
+    missingness : float
+        This is the maximum allowable percentage of missingness expressed as a decimal.  For example a value of 0.25 would mean that all rows for which more than one in four values are missing will be rejected.
+
+    Attributes
+    ----------
+    data : dataframe
+        This is where the raw data read from filename is stored.
+    miss : float
+        This is where the maximum allowable percentage of missingness expressed as a decimal is stored.
+    pats : dict
+        This is where the set of missingness patterns found in the dataset will be stored later, initialized here.
+    """
 
     def __init__(self, filename, missingness):
         """
-        takes input data and missingness threshold and initializes imputable object
+        Constructor, takes input data and missingness threshold and initializes imputable object.
+
+        This is the initialization function for imputation.  It reads the input file of raw data and sets the user specified value for the missingness threshold.
         """
         self.data = pd.read_csv(filename,sep='\t')
         self.miss = float(missingness)
         self.pats = {}
-        self.notdone = True
 
     def deduplicate(self):
         """
-        removes duplicate peptides
+        Removes duplicate peptides.
 
         groups rows by peptide, if a peptide appears in more than one row it is removed
         """
@@ -47,26 +69,34 @@ class imputable:
         self.data = self.data.drop(todrop)
 
     def drop_missing(self):
-        """removes rows which are missing more data than the user specified missingness threshold"""
+        """Removes rows which are missing more data than the user specified missingness threshold."""
         self.miss = np.rint(len(self.data.columns)*self.miss)
         self.data = self.data[self.data.isnull().sum(axis=1)<=self.miss]
 
     def impute(self,outname):
         """
-        imputes missing data with KNN
+        Imputes missing data with KNN and outputs the results to the specified file.
 
-	takes deduplicated data with missing values removed and an output file name
+        First all of the missingness patterns present in the dataset are identified.  Then those patterns are iterated over and for each pattern, missing values are imputed.  Finally the dataset is reformed with the imputed values 
+and output.
 
-        outputs the imputed data to the given filename
+        Parameters
+        ----------
+        outname : str
+            Path to output file.
         """
         def match_pat(l,i):
             """
             finds all missingness patterns present in the dataset
 
-            takes a row of data and its index
+            For each row, if that row has a new missingness pattern, that pattern is added to the list, then whether the missingness pattern is new or not, the index of that row is assigned to the appropriate missingness pattern.
 
-            if that row has a new missingness pattern, that pattern is added to the list
-            whether the missingness pattern is new or not, the index of that row is assigned to the appropriate missingness pattern
+            Parameters
+            ----------
+            l : list
+                A row of data
+            i : int
+                the index of that row in the original dataset
             """
             l = "".join(np.isnan(l).astype(int).astype(str))
             if l not in self.pats.keys():
@@ -75,9 +105,7 @@ class imputable:
                 self.pats[l].append(i)
 
         def get_patterns(arr):
-            """
-            calls match_pat on all rows of data
-            """
+            """Calls match_pat on all rows of data"""
             for ind, val in enumerate(arr):
                 match_pat(val,ind)
 
@@ -85,12 +113,24 @@ class imputable:
             """
             single imputation process for a missingness pattern.
 
-            drops columns missing in a given missingness pattern. Then finds nearest neighbors.  Iterates over rows matching missingness pattern, getting indexes of nearest neighbors, averaging nearest neighbrs and replacing 
+            Drops columns missing in a given missingness pattern. Then finds nearest neighbors.  Iterates over rows matching missingness pattern, getting indexes of nearest neighbors, averaging nearest neighbrs and replacing 
 missing values with corresponding averages.
 
-            takes indexes of rows in missingness pattern, missingness pattern, the original array of data and the array of rows with no missing values
+            Parameters
+            ----------
+            inds : list
+                indexes of rows sharing the missingness pattern.
+            pattern : str
+                Binary representation of missingness pattern.
+            origarr : arr
+                original array of data with missing values
+            comparr : arr
+                Complete array of only rows with no missing values (complete cases).
 
-            returns the matrix of rows matching the missingness pattern with the missing values imputed
+            Returns
+            -------
+            outa : arr
+                Imputed array for missingness pattern.
             """
             #drop missing columns given missingness pattern
             newarr = comparr[:,~np.array(list(pattern)).astype(bool)]
@@ -115,7 +155,19 @@ missing values with corresponding averages.
 
         def imputer(origarr, comparr):
             """
-            calls sub_imputer on each missingness pattern and outputs the results to a dict 
+            Calls sub_imputer on each missingness pattern and outputs the results to a dict.
+
+            Parameters
+            ----------
+            origarr : arr
+                Original array with missing values.
+            comparr : arr
+                Complete array of only rows with no missing values (complete cases).
+
+            Returns
+            -------
+            outdict : dict
+                Dict of imputed data with index in the original dataset as the key and imputed data as the value.
             """
             outdict = {}
             for k in tqdm(self.pats.keys()):
