5cb69c3628d2fb1d5799b5453a22c7f9904bc8b9
aleccrowell <alexader.m.crowell@gmail.com>
2018-01-11 15:36:45 -0500

    missed init.py

--- LIMBR/__init__.py
+++ LIMBR/__init__.py
@@ -5,4 +5,5 @@ from LIMBR import old_fashioned
 """
 The package LIMBR consists of three modules.  imputable performs imputation of missing data using K nearest neighbors.  sva performs modelling and removal of batch effects. old_fashioned provides normalization methods without 
 batch effect removal as a baseline for the results of sva.
+
 """
