7adb0499474a4818f26d73f335505ab5b9fbe267
aleccrowell <alexader.m.crowell@gmail.com>
2018-06-18 12:28:52 -0400

    adding limbr processing

---
+++ LIMBR_docker/New_exp/limbr_process.py
@@ -0,0 +1,17 @@
+from LIMBR import simulations, imputation, batch_fx
+
+for i in range(1, 21):
+    #Read Raw Data
+    to_impute = imputation.imputable('standard_'+str(i)+'_with_noise.txt',0.3)
+    #Impute and Write Output
+    to_impute.impute_data('standard_'+str(i)+'_imputed.txt')
+
+for i in range(1, 21):
+    #Read Imputed Data
+    to_sva = batch_fx.sva(filename='standard_'+str(i)+'_imputed.txt',design='c',data_type='p',pool='pool_map.p')
+    #preprocess data
+    to_sva.preprocess_default()
+    #perform permutation testing
+    to_sva.perm_test(nperm=100)
+    #write_output
+    to_sva.output_default('standard_'+str(i)+'_LIMBR_processed.txt')
