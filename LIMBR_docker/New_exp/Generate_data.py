205fbaa807420a1a4bf3ac5b7c9777d8d252d0e1
aleccrowell <alexader.m.crowell@gmail.com>
2018-06-18 11:05:53 -0400

    fixing range

---
+++ LIMBR_docker/New_exp/Generate_data.py
@@ -0,0 +1,21 @@
+from LIMBR import simulations, imputation, batch_fx
+
+for i in range(1, 21):
+    simulation = simulations.simulate()
+    simulation.generate_pool_map()
+    simulation.write_output('standard_'+str(i))
+
+for i in range(1, 21):
+    simulation = simulations.simulate(amp_noise=1.5)
+    simulation.generate_pool_map()
+    simulation.write_output('double_noise_'+str(i))
+
+for i in range(1, 21):
+    simulation = simulations.simulate(amp_noise=.375)
+    simulation.generate_pool_map()
+    simulation.write_output('half_noise_'+str(i))
+
+for i in range(1, 21):
+    simulation = simulations.simulate(amp_noise=.075)
+    simulation.generate_pool_map()
+    simulation.write_output('tenth_noise_'+str(i))
