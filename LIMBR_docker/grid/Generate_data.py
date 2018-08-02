f68d07e0c0401e00e484d4754d673e48bf29e5b1
aleccrowell <alexader.m.crowell@gmail.com>
2018-08-02 13:10:03 -0400

    consolidate grid simulations

--- LIMBR_docker/grid/Generate_data.py
+++ LIMBR_docker/grid/Generate_data.py
@@ -6,43 +6,12 @@ for i in range(1, 21):
     simulation = simulations.simulate(effect_size=4, p_miss=.4, lam_miss=15, amp_noise=1, rseed=i)
     simulation.generate_pool_map()
     simulation.write_output('twenty_miss_5_NN_'+str(i))
-
-for i in range(1, 21):
-    simulation = simulations.simulate(effect_size=4, p_miss=.4, lam_miss=15, amp_noise=1, rseed=i)
-    simulation.generate_pool_map()
     simulation.write_output('twenty_miss_10_NN_'+str(i))
-
-for i in range(1, 21):
-    simulation = simulations.simulate(effect_size=4, p_miss=.4, lam_miss=15, amp_noise=1, rseed=i)
-    simulation.generate_pool_map()
     simulation.write_output('twenty_miss_15_NN_'+str(i))
-
-for i in range(1, 21):
-    simulation = simulations.simulate(effect_size=4, p_miss=.4, lam_miss=15, amp_noise=1, rseed=i)
-    simulation.generate_pool_map()
     simulation.write_output('thirty_miss_5_NN_'+str(i))
-
-for i in range(1, 21):
-    simulation = simulations.simulate(effect_size=4, p_miss=.4, lam_miss=15, amp_noise=1, rseed=i)
-    simulation.generate_pool_map()
+    simulation.write_output('thirty_miss_5_NN_'+str(i))
     simulation.write_output('thirty_miss_10_NN_'+str(i))
-
-for i in range(1, 21):
-    simulation = simulations.simulate(effect_size=4, p_miss=.4, lam_miss=15, amp_noise=1, rseed=i)
-    simulation.generate_pool_map()
     simulation.write_output('thirty_miss_15_NN_'+str(i))
-
-for i in range(1, 21):
-    simulation = simulations.simulate(effect_size=4, p_miss=.4, lam_miss=15, amp_noise=1, rseed=i)
-    simulation.generate_pool_map()
     simulation.write_output('forty_miss_5_NN_'+str(i))
-
-for i in range(1, 21):
-    simulation = simulations.simulate(effect_size=4, p_miss=.4, lam_miss=15, amp_noise=1, rseed=i)
-    simulation.generate_pool_map()
     simulation.write_output('forty_miss_10_NN_'+str(i))
-
-for i in range(1, 21):
-    simulation = simulations.simulate(effect_size=4, p_miss=.4, lam_miss=15, amp_noise=1, rseed=i)
-    simulation.generate_pool_map()
     simulation.write_output('forty_miss_15_NN_'+str(i))
