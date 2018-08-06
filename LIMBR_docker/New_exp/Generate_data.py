8650710178d73b802900c3ba42733e9e1d6d198b
aleccrowell <alexader.m.crowell@gmail.com>
2018-08-06 14:37:50 -0400

    tweaking simulations values

--- LIMBR_docker/New_exp/Generate_data.py
+++ LIMBR_docker/New_exp/Generate_data.py
@@ -1,21 +1,21 @@
 from LIMBR import simulations, imputation, batch_fx
 
 for i in range(1, 21):
-    simulation = simulations.simulate(p_miss=.4, lam_miss=15, phase_noise=.5, amp_noise=1.5, rseed=i)
+    simulation = simulations.simulate(effect_size=0.25, p_miss=.4, lam_miss=15, phase_noise=.5, amp_noise=1.5, rseed=i)
     simulation.generate_pool_map()
     simulation.write_output('standard_'+str(i))
 
 for i in range(1, 21):
-    simulation = simulations.simulate(effect_size=4,p_miss=.4, lam_miss=15, phase_noise=.5, amp_noise=1.5, rseed=i)
+    simulation = simulations.simulate(effect_size=0.5, p_miss=.4, lam_miss=15, phase_noise=.5, amp_noise=1.5, rseed=i)
     simulation.generate_pool_map()
     simulation.write_output('double_noise_'+str(i))
 
 for i in range(1, 21):
-    simulation = simulations.simulate(effect_size=1,p_miss=.4, lam_miss=15, phase_noise=.5, amp_noise=1.5, rseed=i)
+    simulation = simulations.simulate(effect_size=0.125, p_miss=.4, lam_miss=15, phase_noise=.5, amp_noise=1.5, rseed=i)
     simulation.generate_pool_map()
     simulation.write_output('half_noise_'+str(i))
 
 for i in range(1, 21):
-    simulation = simulations.simulate(effect_size=3, p_miss=.4, lam_miss=15, phase_noise=.5, amp_noise=1.5, rseed=i)
+    simulation = simulations.simulate(effect_size=.0625, p_miss=.4, lam_miss=15, phase_noise=.5, amp_noise=1.5, rseed=i)
     simulation.generate_pool_map()
     simulation.write_output('tenth_noise_'+str(i))
